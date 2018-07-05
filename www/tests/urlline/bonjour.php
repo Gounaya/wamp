0dc111eab42e0016a174fc14d9d0c0eee4e5653d
Gounaya <o.gounaya@gmail.com>
2018-07-06 00:11:32 +0200

    last add for today ! finish the url part

--- www/tests/urlline/bonjour.php
+++ www/tests/urlline/bonjour.php
@@ -10,9 +10,14 @@
          
         <?php 
         if(isset($_GET['nom']) AND isset($_GET['prenom']) AND isset($_GET['repeter'])) {
-            for($rep = 1; $rep<=$_GET['repeter']; $rep++){
-                echo 'Bonjour '. $_GET['nom'] .' '. $_GET['prenom'];
-                '<br>'
+            $nbRepetition = (int) $_GET['repeter'];
+            if($nbRepetition <100)
+            {
+                for($rep = 0; $rep<=$nbRepetition; $rep++){
+                    echo 'Bonjour '. $_GET['nom'] .' '. $_GET['prenom'].'<br>';
+                }
+            }else{
+                echo 'warning: you should put a number between 0 and 100 !'
             }
         }else{
             echo 'undefine name or lastname !';
