8157c8551799daeedf8cc1cf37706e6f8b4808f0
Gounaya <o.gounaya@gmail.com>
2018-07-05 14:16:07 +0200

    add a php file

---
+++ www/tests/urlline/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>test data in url</title>
+
+    </head>
+    <body>
+        <a href="bonjour.php?nom=Gounaya&amp;prenom=Oussama">
+            Dis-moi bonjour !</a>
+    </body>
+
+</html>
