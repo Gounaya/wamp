afce8f35fbf18618599f8e98bf94e561bc60c769
Gounaya <o.gounaya@gmail.com>
2018-07-05 14:27:12 +0200

    add new file who display my name and my lastname

---
+++ www/tests/urlline/bonjour.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>test data in url</title>
+
+    </head>
+    <body>
+    <p>
+        Bonjour <?php echo $_GET['no']
+    </p>
+    </body>
+
+</html>
