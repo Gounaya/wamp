4e91b96e307b82f799a16683833a10a96d055053
Gounaya <o.gounaya@gmail.com>
2018-07-05 15:44:40 +0200

    add everythings  I lost the last modification

--- www/tests/urlline/bonjour.php
+++ www/tests/urlline/bonjour.php
@@ -7,7 +7,17 @@
     </head>
     <body>
     <p>
-        Bonjour <?php echo $_GET['no']
+         
+        <?php 
+        if(isset($_GET['nom']) AND isset($_GET['prenom']) AND isset($_GET['repeter'])) {
+            for($rep = 1; $rep<=$_GET['repeter']; $rep++){
+                echo 'Bonjour '. $_GET['nom'] .' '. $_GET['prenom'];
+                '<br>'
+            }
+        }else{
+            echo 'undefine name or lastname !';
+        }
+        ?>
     </p>
     </body>
 
